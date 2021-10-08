3e429007efb4cf83442cc276ef96c0d998c6ae2f
Arzmuf <arizmuf13@gmail.com>
2021-10-08 15:28:27 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h2>HAIHAI</h2>
+    <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, repellendus quasi! Tenetur numquam suscipit asperiores temporibus eligendi. Necessitatibus pariatur sapiente nemo. Nihil vero excepturi distinctio! Voluptatibus nostrum neque voluptates quisquam.  </P>
 </body>
 </html>
